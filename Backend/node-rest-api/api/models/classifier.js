7cacfc5c9ad21b4e65425b24b809462885d75841
Kevin Wang <kevin@Kevin-2.local>
2018-02-18 22:43:26 -0800

    ASdf

--- Backend/node-rest-api/api/models/classifier.js
+++ Backend/node-rest-api/api/models/classifier.js
@@ -4,40 +4,17 @@ const mongoose = require('mongoose');
 
 const classifierSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    ownerId: {
-        type: Number, 
-        required: true
-    },
-    description: {
-        type: String, 
-        required: true
-    },
-    trainingsSets: [
-        { type: mongoose.Schema.Types.ObjectId }
-    ],
-    subscriberIds: [
-        { type: mongoose.Schema.Types.objectId }
-    ],
-    isPublic: {
-        type: Boolean, 
-        default: false, 
-        required: true
-    },
-    index: {
-        // index for tensorflow
-        type: mongoose.Schema.Types.objectId,
-        required: true
-    },
+    ownerId: { type: Number, required: true },
+    description: { type: String, required: true },
+    trainingsSets: [ { type: mongoose.Schema.Types.ObjectId } ],
+    subscriberIds: [ { type: mongoose.Schema.Types.objectId } ],
+    isPublic: { type: Boolean, default: false, required: true },
+    // index for tensorflow
+    index: { type: mongoose.Schema.Types.objectId, required: true },
     nodes: [
         {
-            name: {
-                type: String, 
-                required: true
-            },
-            index: {
-                type: Number, 
-                required: true
-            }
+            name: { type: String, required: true },
+            index: { type: Number, required: true }
         }
     ]
 });
