04063eff46c92f8f46d45b2cb39f5eb34507c73e
Kevin Wang <kevin@Kevin-2.local>
2018-02-18 22:05:40 -0800

    added more things

--- Backend/node-rest-api/api/models/user.js
+++ Backend/node-rest-api/api/models/user.js
@@ -9,8 +9,9 @@ const userSchema = mongoose.Schema({
         match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/ 
     },
     password: { type: String, require: true },
-    name: { type: String, required: true }
-
+    name: { type: String, required: true },
+    organization: { type: String, required: true }//,
+    // classifiers: [{ type: mongoose.Schema.Types.objectId, ref: 'Classifier' }]
 });
 
 module.exports = mongoose.model('User', userSchema)
