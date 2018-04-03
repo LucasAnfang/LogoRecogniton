4e628592bacb6c5895cd8e1e3ad400437c50156e
Kevin Wang <wangkj@usc.edu>
2018-04-03 16:46:57 -0700

    tensorflow updates

--- Backend/node-rest-api/api/routes/tensorflow.js
+++ Backend/node-rest-api/api/routes/tensorflow.js
@@ -5,5 +5,7 @@ const checkAuth = require('../middleware/check-auth');
 const TensorflowController = require('../controllers/tensorflow');
 
 router.get('/training', checkAuth, TensorflowController.fetch_new_batches_and_set);
+router.post('/checkpoints', checkAuth, TensorflowController.store_checkpoints);
+router.post('/results', checkAuth, TensorflowController.store_results);
 
 module.exports = router;
