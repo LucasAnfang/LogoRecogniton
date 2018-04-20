b9fbbe375a365508fc1843c528ab4ea7af37e6e4
Kevin Wang <wangkj@usc.edu>
2018-04-20 16:46:16 -0700

    changed image model

--- Backend/node-rest-api/api/models/image.js
+++ Backend/node-rest-api/api/models/image.js
@@ -5,13 +5,10 @@ const imageSchema = mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, required: true },
     parentDatasetId: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'Dataset'},
     url: { type: String, required: true },
-    results: {
-        classifier: { type:String },
-        results: [{
-                type: { type: String, required: true},
-                values: [{ type: String, required: true}],
-        }]
-    }
+    results: [{
+        classifier: { type: String },
+        results: {type: String, required: true}
+    }]
 });
 
 module.exports = mongoose.model('ImageObj', imageSchema);
